fe85f01cf77b462b99ff95b474df0d6ef75ea7ca
Kyle Geppert <krgeppert@gmail.com>
2013-05-15 08:59:16 -0500

    Wrote Skeleton Code, Variables and Basic Organization

---
+++ data_handler.js
@@ -0,0 +1,68 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: kyle
+ * Date: 5/15/13
+ * Time: 8:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    // Set up Data.
+    var streamType = "home";
+    var date = new Date();
+    var updateStamp = date.getTime();
+    var $body = $('body');
+    var $listOfTweets = $('<ul></ul>');
+    var $updateButton = $('<button id="update" type="button" onclick="updateStream()" >Update</button>');
+    var $homeButton = $('<button id="toHome" type="button" onclick="goHome()" >Home</button>');
+    // Set up Functions.
+    var goHome = function(){
+    };
+    var organizePage = function(){
+        $listOfTweets.appendTo($body);
+        $updateButton.appendTo($body);
+        $homeButton.appendTo($body);
+
+    };
+    var displayTweets = function(){
+    };
+    var generateTweet = function(){
+    };
+    var updateStream = function(){
+    };
+
+    // Implement page.
+
+    $body.html('');  // What does this do?
+    organizePage();
+    displayTweets();
+
+
+
+
+
+
+//    var index = streams.home.length - 1;
+//    while(index >= 0){
+//        var tweet = streams.home[index];
+//        var $tweet = $('<div></div>');
+//        $tweet.text('@' + tweet.user + ': ' + tweet.message + " created at: " + tweet.timeStamp);
+//        $tweet.appendTo($body);
+//        index -= 1;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
